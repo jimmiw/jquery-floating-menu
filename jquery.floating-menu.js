981894c0321ceb051ffc48920afb935251c3d938
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2011-06-16 13:05:54 +0200

    Added the basic functionality of the script

---
+++ jquery.floating-menu.js
@@ -0,0 +1,19 @@
+(function($) {
+  $.fn.floatmenu = function(options) {
+    return this.each(function() {
+      // fetches and initializes the current element.
+      var obj = $(this);
+      
+      /*
+      $(window).scroll(function(e) {
+        if($(window).scrollTop() > 215) {
+          $('#jq-interiorNavigation').css('position','fixed').css('top','10px');
+        }
+        else {
+          $('#jq-interiorNavigation').css('position','static');
+        }
+      });
+      */
+    }
+  };
+})(jQuery);
